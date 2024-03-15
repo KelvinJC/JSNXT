a86ea936f8ce14648e64447a207471d6a285afdc
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-03-15 12:10:30 +0100

    Add intro to exercise

--- eloquent/ex1_loop_triangle.js
+++ eloquent/ex1_loop_triangle.js
@@ -1,3 +1,18 @@
+/*
+
+Exercise 1:
+Looping a triangle
+Write a loop that makes seven calls to `console.log` to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+
+*/
 
 for (let triangle_line = "#"; triangle_line.length < 8; triangle_line += "#") {
     console.log(triangle_line);
