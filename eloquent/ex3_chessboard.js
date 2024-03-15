050b503fe4909461226519f9d6c339685fc7258b
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-03-15 20:00:27 +0100

    edit intro

--- eloquent/ex3_chessboard.js
+++ eloquent/ex3_chessboard.js
@@ -17,8 +17,8 @@ Passing this string to console.log should show something like this:
 # # # #
 
 When you have a program that generates this pattern, 
-define a binding size = 8 and
-change the program so that it works for any size, 
+define a binding size = 8 and change the program
+so that it works for any size, 
 outputting a grid of the given width and height.
 
 */
