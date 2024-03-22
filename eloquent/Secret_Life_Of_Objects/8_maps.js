57d67fec62063854aca56352a737c76c798ac71c
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-03-22 12:24:24 +0100

    Use object without prototype as map

--- eloquent/Secret_Life_Of_Objects/8_maps.js
+++ eloquent/Secret_Life_Of_Objects/8_maps.js
@@ -15,4 +15,33 @@ console.log(`Julia is ${ages["Julia"]}`);
 // -> Julia is 62
 console.log("Is Jack's age known?", "Jack" in ages);
 console.log("Is Boris's age known?", "Boris" in ages);
-console.log("Is toString's age known?", "toString" in ages);
+console.log("Is toString's age known?", "toString" in ages); // ages.hasOwnProperty("toString") to ignore derived properties of object prototype
+
+/*
+Here, the object’s property names are the people’s names, 
+and the property values are their ages. 
+But we certainly didn’t list anybody named toString in our map. 
+Yet, because plain objects derive from Object.prototype, 
+it looks like the property is there.
+
+As such, using plain objects as maps is dangerous. 
+There are several possible ways to avoid this problem. 
+
+First, it is possible to create objects with no prototype. 
+If you pass `null` to `Object.create`, the resulting object will not
+derive from `Object.prototype` and can safely be used as a map.
+*/
+
+let protolessAges = Object.create(null);
+
+protolessAges.Boris = 39
+protolessAges.Liang = 22
+protolessAges.Julia = 62
+protolessAges.addAges = function() {return this.Boris + this.Julia + this.Liang;}
+
+console.log(`Julia is ${protolessAges["Julia"]}`);
+console.log("Is Jack's age known?", "Jack" in protolessAges);
+console.log("Is Boris's age known?", "Boris" in protolessAges);
+console.log("Is toString's age known?", "toString" in protolessAges); 
+console.log(protolessAges.addAges());
+
