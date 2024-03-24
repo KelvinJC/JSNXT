75453fd5858f050a561bb80cf8cee04ec08f6751
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-03-24 16:21:06 +0100

    Edit comment

--- eloquent/robot_project_modules/test_route_robot.js
+++ eloquent/robot_project_modules/test_route_robot.js
@@ -14,4 +14,4 @@ function routeRobot(state, memory) {
     return {direction: memory[0], memory: memory.slice(1)}
 }
 
-runRobot(VillageState.random(), routeRobot, []); // test with routeRobot
+runRobot(VillageState.random(), routeRobot, []); // test routeRobot
