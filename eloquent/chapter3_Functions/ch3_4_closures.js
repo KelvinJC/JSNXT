0a70621e25820232ffed5b686eb9581a95e187e9
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-03-25 10:50:16 +0100

    Practice closures

---
+++ eloquent/chapter3_Functions/ch3_4_closures.js
@@ -0,0 +1,14 @@
+function wrapValue(n) {
+    let local = n;
+    return () => local;
+}
+
+let wrap1 = wrapValue(1);
+console.log(wrap1);
+let wrap2 = wrapValue(2);
+console.log(wrap2);
+
+console.log(wrap1());
+console.log(wrap2());
+
+
